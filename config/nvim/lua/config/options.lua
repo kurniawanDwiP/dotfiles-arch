675733d050701fee697e67f1401ce083efef0e15
kurniawan dwi prasetyo <kurniawandwiprasetyo1201@gmail.com>
2025-10-18 20:11:46 +0700

    change power menu from gtk module to waybar custom,add window rule on niri to block some sensitive apps from screen share

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,6 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.lsp.enable("ts_ls", true)
-vim.g.lazyvim_prettier_needs_config = false
-vim.g.autoformat = true
