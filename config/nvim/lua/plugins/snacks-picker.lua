5133cb634f60ed61c3b68569f0d4d7499c4b6bdf
kurniawan dwi prasetyo <kurniawandwiprasetyo1201@gmail.com>
2025-11-14 01:14:43 +0700

    add ts-autotag and change from telescope to snacks picker

---
+++ config/nvim/lua/plugins/snacks-picker.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        layout = "custom",
+        layouts = {
+          custom = {
+            layout = {
+              box = "vertical",
+              backdrop = false,
+              row = -1,
+              width = 0,
+              height = 0.2,
+              border = "rounded",
+              title = " {title} {live} {flags}",
+              title_pos = "left",
+              {
+                box = "horizontal",
+                { win = "list", border = "none" },
+                { win = "preview", title = "{preview}", width = 0.6, border = "left" },
+              },
+              { win = "input", height = 1, border = "bottom" },
+            },
+          },
+        },
+        source = {
+          grep = {},
+        },
+      },
+    },
+  },
+}
