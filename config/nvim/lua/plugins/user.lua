2ba28783dffcb1efa5277fedeffa3869c21465e6
kurniawan dwi prasetyo <kurniawandwiprasetyo1201@gmail.com>
2025-11-28 16:07:15 +0700

    fix something

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -1,6 +1,18 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "rebelot/kanagawa.nvim" },
+  {
+    "vague-theme/vague.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        -- optional configuration here
+      })
+      vim.cmd("colorscheme vague")
+    end,
+  },
   { "kepano/flexoki-neovim", name = "flexoki" },
   {
     "folke/tokyonight.nvim",
