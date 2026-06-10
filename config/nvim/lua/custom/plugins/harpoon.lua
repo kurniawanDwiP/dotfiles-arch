6b4668ac93007aaa2da0f8dae6a44c55e80c167b
Kurniawan Dwi Prasetyo <kurniawandwiprasetyo1201@gmail.com>
2026-06-11 02:40:11 +0700

    feat: implement new hrm on kanata and add autocommand for harpoon

--- config/nvim/lua/custom/plugins/harpoon.lua
+++ config/nvim/lua/custom/plugins/harpoon.lua
@@ -4,6 +4,22 @@ return {
     lazy = false,
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "harpoon",
+        callback = function(args)
+          local bufnr = args.buf
+          for i = 1, 9 do
+            vim.keymap.set("n", tostring(i), function()
+              vim.api.nvim_win_set_cursor(0, { i, 0 })
+              harpoon.ui:select_menu_item(harpoon:list())
+            end, { buffer = bufnr, silent = true, desc = "Harpoon select " .. i })
+          end
+        end,
+      })
+    end,
     opts = {
       menu = {
         width = vim.api.nvim_win_get_width(0) - 4,
